ee031642a34e51ed6388203b4f5b80374c0d556f
minkyngkm <min.kim@canonical.com>
2024-01-03 14:45:43 +0000

    Test reset fields by click cancel button

--- tests/playwright/tests/checkout.spec.ts
+++ tests/playwright/tests/checkout.spec.ts
@@ -41,4 +41,73 @@ test.describe("Checkout - Region and taxes", () => {
     expect(await page.$('[data-testid="total"]')).toBeNull();
     expect(await page.$('[data-testid="tax"]')).toBeNull();
   })
+
+  test("Click cancel should reset fields", async ({page}) => {
+    await page.goto("/pro/subscribe")
+    await acceptCookiePolicy(page)
+    await selectProducts(page);
+    await page.getByRole("button", { name: "Buy now" }).click();
+
+    await login(page);
+
+    await page.locator(
+      ":nth-child(1) > .p-stepped-list__content > .row > .u-align--right > .p-action-button"
+    ).click(); // Click "Edit" button
+    await page.getByLabel("Country/Region:").selectOption({ label: 'France' })
+    await page.getByLabel("VAT number:").fill("ABDCDEFG")
+
+    // Click cancel button
+    await page.locator( ":nth-child(1) > .p-stepped-list__content > :nth-child(1) > .u-align--right > :nth-child(1)").click();
+    
+    const country = await page.$('[data-testid="country"]')
+    const countryText = await country?.innerText();
+    const vatNumber = await page.$('[data-testid="vat-number"]')
+    const vatNumberText =  await vatNumber?.innerText();
+
+    expect(countryText).toBe("United Kingdom");
+    expect(vatNumberText).toBe("None");
+
+    // Click 1st Edit button
+    await page.locator(
+      ":nth-child(1) > .p-stepped-list__content > .row > .u-align--right > .p-action-button"
+    ).click(); // Click "Edit" button
+    await page.getByLabel("Country/Region:").selectOption({ label: 'Canada' })
+    await page.getByLabel("Province:").selectOption({ label: 'Alberta' })
+
+     // Click cancel button
+    await page.locator( ":nth-child(1) > .p-stepped-list__content > :nth-child(1) > .u-align--right > :nth-child(1)").click();
+    
+    expect(countryText).toBe("United Kingdom");
+    expect(vatNumberText).toBe("None");
+  })
+})
+
+test.describe("Checkout - Your inforamtion", ()=>{
+  test("Click cancel should reset field", async ({page}) => {
+    await page.goto("/pro/subscribe")
+    await acceptCookiePolicy(page)
+    await selectProducts(page);
+    await page.getByRole("button", { name: "Buy now" }).click();
+
+    await login(page);
+    
+    await expect(page).toHaveURL('/account/checkout');
+
+    await page.locator( ":nth-child(3) > .p-stepped-list__content > :nth-child(1) > .u-align--right > .p-action-button").click(); // Click Edit button
+
+    await page.getByLabel("Name:").fill("Abcd")
+    await page.getByLabel("Address:").fill("Abcd");
+    await page.getByLabel("City:").fill("Abcd");
+    await page.getByLabel("Postal code:").fill("Abcd");
+    await page.locator( ":nth-child(3) > .p-stepped-list__content > :nth-child(1) > .u-align--right > :nth-child(1)").click(); // Click cancel button
+
+    const customerName = await page.locator("data-testid=customer-name").innerText();
+    const customerAddress = await page.locator("data-testid=customer-address").innerText();
+    const customerCity = await page.locator("data-testid=customer-city").innerText();;
+    const customerPostalCode = await page.locator("data-testid=customer-postal-code").innerText();;
+    expect(customerName).not.toContain("Abcd");
+    expect(customerAddress).not.toContain("Abcd");
+    expect(customerCity).not.toContain("Abcd");
+    expect(customerPostalCode).not.toContain("Abcd");
+  })
 })
