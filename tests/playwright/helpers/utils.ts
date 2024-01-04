e154f9d4f521ba3f007141739f29509c99fc0e97
minkyngkm <min.kim@canonical.com>
2024-01-04 14:32:18 +0000

    Write users spect for /pro/users

--- tests/playwright/helpers/utils.ts
+++ tests/playwright/helpers/utils.ts
@@ -2,3 +2,6 @@ export const ENDPOINTS = {
    customerInfo: "/account/customer-info*",
    preview: "/pro/purchase/preview*"
  };
+
+export const getRandomEmail = () =>
+  `playwright-test-${Math.random().toString(36).substring(2,12)}@canonical.com`;
